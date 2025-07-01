30156b7dc4cf6358c0c9f752da542edf894a3df5
Enzo Crisóstomo <enzo.crisostomo2000@gmail.com>
2025-06-30 22:29:10 -0300

    Exemplo carregar tiles

--- exemplo_mapa.cpp
+++ exemplo_mapa.cpp
@@ -37,6 +37,11 @@ void StageState::LoadTMX(std::string file)
                     std::cout << "Object class" << object.getClass() << std::endl;
                     const auto& properties = object.getProperties();
                     std::cout << "Object has " << properties.size() << " properties" << std::endl;
+                    if(object.getClass() == "Chainsaw"){
+                      
+                    }
+
+                    if(object.getClass() )
                     for (const auto& prop : properties)
                     {
                         std::cout << "Found property: " << prop.getName() << std::endl;
@@ -53,5 +58,49 @@ void StageState::LoadTMX(std::string file)
     else
     {
         std::cout << "Failed loading map" << std::endl;
+    }
+}
+
+TileMap::TileMap(GameObject& associated, TileSet* tileSet, const tmx::Map& map) : Component(associated)
+{
+    m_tileSet = tileSet;
+    Load(map);
+}
+
+void TileMap::Load(const tmx::Map& map)
+{
+    const auto& layers = map.getLayers();
+    std::cout << "Map has " << layers.size() << " layers" << std::endl;
+    for (const auto& layer : layers)
+    {
+        m_mapWidth = layer->getSize().x;
+        m_mapHeight = layer->getSize().y;
+        m_mapDepth = 1;
+        if (layer->getType() == tmx::Layer::Type::Tile)
+        {
+            if (layer->getName().find("") == std::string::npos) {
+                continue;
+            }
+            const auto& tiles = layer->getLayerAs<tmx::TileLayer>().getTiles();
+            if (tiles.empty())
+            {
+                const auto& chunks = layer->getLayerAs<tmx::TileLayer>().getChunks();
+                if (chunks.empty())
+                {
+                    std::cout << "Layer has missing tile data\n";
+                }
+                else
+                {
+                    std::cout << "Layer has " << chunks.size() << " tile chunks.\n";
+                }
+            }
+            else
+            {
+                std::cout << "Layer has " << tiles.size() << " tiles.\n";
+                for (auto& tile : tiles) {
+                    m_tileMatrix.push_back(tile.ID - 1);
+                }
+            }
+        }
     }
 }

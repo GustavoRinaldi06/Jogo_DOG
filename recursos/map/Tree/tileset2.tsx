7a9448477bd04319ccbbe52fcd025c2065cc2c2b
me-luc <lukasalmeida372@gmail.com>
2025-07-13 02:22:15 -0300

    reorganizando arquivos

--- recursos/map/Tree/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset2" tilewidth="500" tileheight="500" tilecount="20" columns="5">
- <image source="../../img/Tree/tile.png" width="2500" height="2000"/>
-</tileset>

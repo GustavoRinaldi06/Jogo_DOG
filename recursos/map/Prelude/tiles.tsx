24740e594c529ec186728d18216559eeb2dfffe4
me-luc <lukasalmeida372@gmail.com>
2025-07-13 21:56:09 -0300

    remover arquivo desnecassario

--- recursos/map/Prelude/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="250" tileheight="250" tilecount="20" columns="5">
- <image source="../Tree/tiles.png" width="1250" height="1000"/>
-</tileset>

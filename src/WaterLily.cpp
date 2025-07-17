10d4ac2b68f41d24c151abe65edb6c32d254bfb3
me-luc <lukasalmeida372@gmail.com>
2025-07-17 00:08:28 -0300

    ajustes waterlily gravidade + tamanho

--- src/WaterLily.cpp
+++ src/WaterLily.cpp
@@ -4,24 +4,34 @@
 #include "Animator.h"
 
 WaterLily::WaterLily(GameObject &associated)
-    : Component(associated)
+    : Component(associated), gravity(800.0f),
+      isOnGround(false), applyGravity(true), isFalling(false)
 {
-    auto renderer = new SpriteRenderer(associated, "recursos/map/Animal/tiles.png", 5, 4);
+    auto renderer = new SpriteRenderer(associated, "recursos/img/sprites/WaterLily.png", 1, 1);
     renderer->SetCameraFollower(false);
     associated.AddComponent(renderer);
 
     auto animator = new Animator(associated);
-    animator->AddAnimation("t-on", Animation(17, 17, 0.5f));
+    animator->AddAnimation("t-on", Animation(1, 1, 0.5f));
     associated.AddComponent(animator);
 
-    associated.AddComponent(new Collider(associated));
+    auto collider = new Collider(associated);
+    collider->tag = "waterlily";
+    associated.AddComponent(collider);
     animator->SetAnimation("t-on");
+    speed = Vec2(0, 0);
 }
 
 void WaterLily::Update(float dt)
 {
-    //speed.y += gravity * dt;
-    //associated.box.y += speed.y * dt;
+    if (applyGravity && !isOnGround) {
+        speed.y += gravity * dt;
+        associated.box.y += speed.y * dt;
+        isFalling = true;
+    }
+    else {
+        isFalling = false;
+    }
 }
 
 void WaterLily::Render()
@@ -36,5 +46,13 @@ bool WaterLily::Is(const std::string &type)
 
 void WaterLily::NotifyCollision(GameObject &other)
 {
-    // No special collision logic for now
+    Collider* otherCollider = (Collider*)other.GetComponent("Collider");
+    if (otherCollider && otherCollider->tag == "ground") {
+        isOnGround = true;
+        speed.y = 0;  // Para o movimento vertical
+        
+        // Ajusta a posição para ficar sobre o chão
+        Rect& box = associated.box;
+        box.y = other.box.y - box.h;
+    }
 }
